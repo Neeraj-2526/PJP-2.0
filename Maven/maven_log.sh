6ac3fe3c77975db46836dd785de543aaac460589
Neeraj Kumar <nkneerajkumar2526@gmail.com>
2020-08-23 00:36:31 +0530

    Maven Bash Files

---
+++ Maven/maven_log.sh
@@ -0,0 +1,11 @@
+echo "Enter the file name to caplture logs"
+
+read log
+
+echo "Enter the maven command : "
+
+read command 
+
+$command -l
+
+echo "Log file generated"
---
+++ Maven/maven_offline.sh
@@ -0,0 +1,5 @@
+echo "Enter the maven command : "
+
+read command 
+
+$command -o
